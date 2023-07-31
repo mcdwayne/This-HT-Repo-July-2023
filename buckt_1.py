954773bdf44612c62c0165075e2d32ef42669389
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2023-07-31 17:44:23 -0500

    swapping the creds

--- buckt_1.py
+++ buckt_1.py
@@ -6,7 +6,7 @@ import aws_lib
 
 
 def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAVAGUQ5JEZC5D2FQG", "FhgmJTQly+nIgAPeN0+kx6Mx/8Elpos5UzqK3zj")
+    database = aws_lib.connect("AKIAVAGUQ5JEZC5D4FQG", "MhgmJTQly+nIgAPeN0+kx6Mx/8Elpos5UzqK3zj")
     database.push(data)
 
 
