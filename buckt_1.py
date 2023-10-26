02c8760f5ad8702434d408295cc9ac6960728675
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2023-10-26 07:01:23 -0700

    Update buckt_1.py

--- buckt_1.py
+++ buckt_1.py
@@ -9,4 +9,5 @@ def aws_upload(data: Dict):
     database = aws_lib.connect("AKIAVAGUQ5JEZC5D4FQG", "MhgmJTQly+nIgAPeN0+kx6Mx/8Elpos5UzqK3zj")
     database.push(data)
 
-
+# aws_access_key_id = AKIAWY56YWJJZRJDL2N4
+# aws_secret_access_key = 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
