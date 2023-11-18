7001521c89109fcf2d94f01c33b7b3f6820f0a85
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2023-11-18 15:03:23 -0500

    Update buckt_1.py

--- buckt_1.py
+++ buckt_1.py
@@ -1,14 +1,1 @@
-# Save data to an AWS bucket
 
-from typing import Dict
-
-import aws_lib
-
-
-# def aws_upload(data: Dict):
-#     database = aws_lib.connect("AKIAVAGUQ5JEZC5D4FQG", "MhgmJTQly+nIgAPeN0+kx6Mx/8Elpos5UzqK3zj")
-#     database.push(data)
-
-
-# aws_access_key_id = AKIAVD32IN6IEST5ZQ4U
-# aws_secret_access_key = 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
